16f4c7023310835dc70926ed611dd8ec6dd0a1f8
sumi-re01 <owob.viola@gmail.com>
2021-02-10 03:52:28 +0000

    [Fix]header link

--- Bookers2/app/controllers/application_controller.rb
+++ Bookers2/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  # ログイン前
   before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
