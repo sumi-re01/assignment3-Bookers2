fae68b5552e78c1a6671c6f56b2ad05bbafce268
sumi-re01 <owob.viola@gmail.com>
2021-02-11 02:32:01 +0000

    [Fix]books_controller => Books#show user info

--- Bookers2/app/controllers/books_controller.rb
+++ Bookers2/app/controllers/books_controller.rb
@@ -1,37 +1,39 @@
 class BooksController < ApplicationController
+  # book詳細画面の新期投稿フォームにはBook.new,詳細にはBook.find(parama[:id])を入れたい
+  # Book.new =>@book_newで統一。
 
   def show
-    # book詳細画面の新期投稿フォームにはBook.new,詳細にはBook.find(parama[:id])を入れたい
-    @user = current_user
-    @book = Book.new
-    # ↓要編集
-    @booka = Book.find(params[:id])
+    @book_new = Book.new
+    # ↓要編集 book詳細
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
     @user = current_user
-    @book = Book.new
+    @book_new = Book.new
     @books = Book.all
   end
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
+    @book_new = Book.new(book_params)
+    @book_new.user_id = current_user.id
+    if @book_new.save
       flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book)
+      redirect_to book_path(@book_new)
     else
       @user = current_user
       @books = Book.all
+      # render先はいつもbooks#indexにいく。
       render :index
     end
   end
 
   def edit
     @book = Book.find(params[:id])
-    if @book.user == current_user
-      render"edit"
-    else
+    if @book.user != current_user
       redirect_to books_path
+    else
+      render"edit"
     end
   end
   def update
--- Bookers2/app/controllers/users_controller.rb
+++ Bookers2/app/controllers/users_controller.rb
@@ -1,22 +1,22 @@
 class UsersController < ApplicationController
+    # Book.new =>@book_newで統一。
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    @book = Book.new
+    @book_new = Book.new
   end
 
   def index
     @user = current_user
     @users = User.all
-    @book = Book.new
+    @book_new = Book.new
   end
-
 
   def edit
     @user = User.find(params[:id])
     if @user != current_user
-       redirect_to user_path(@current_user)
+       redirect_to user_path(current_user)
     else
        render :edit
     end
